87f51fdb009d71fa94eada9d0b1b4ce8de13b6ff
Bastian Kauschke <bastian_kauschke@hotmail.de>
2020-02-14 18:52:32 +0100

    start refactoring akari_core

--- akari_core/src/lib.rs
+++ akari_core/src/lib.rs
@@ -9,6 +9,8 @@ extern crate thread_profiler;
 #[macro_use]
 extern crate log;
 
+use std::path::Path;
+
 use crow::{
     glutin::{EventsLoop, Icon, WindowBuilder},
     image, Context, DrawConfig, Texture,
@@ -32,16 +34,75 @@ use crate::{
     systems::{draw, Systems},
 };
 
-const ARENA_WIDTH: usize = 16;
-const ARENA_HEIGHT: usize = 12;
-const GAME_SIZE: (u32, u32) = (20 * ARENA_WIDTH as u32, 20 * ARENA_HEIGHT as u32);
-const WINDOW_SCALE: u32 = 3;
-const FPS: u32 = 60;
+pub const ARENA_WIDTH: usize = 16;
+pub const ARENA_HEIGHT: usize = 12;
+pub const GAME_SIZE: (u32, u32) = (20 * ARENA_WIDTH as u32, 20 * ARENA_HEIGHT as u32);
+pub const WINDOW_SCALE: u32 = 3;
+pub const FPS: u32 = 60;
+
+pub struct GlobalState {
+    pub s: Systems,
+    pub r: Ressources,
+    pub c: Components,
+}
+
+impl GlobalState {
+    pub fn new(fps: u32) -> Self {
+        GlobalState {
+            s: Systems::new(),
+            r: Ressources::new(fps),
+            c: Components::new(),
+        }
+    }
+
+    pub fn run<F>(self, ctx: &mut Context, mut f: F) -> Result<(), crow::Error>
+    where
+        F: FnMut(
+            &mut Context,
+            &mut Texture,
+            &mut Systems,
+            &mut Ressources,
+            &mut Components,
+        ) -> Result<bool, crow::Error>,
+    {
+        let (mut s, mut r, mut c) = (self.s, self.r, self.c);
+
+        let mut surface = ctx.window_surface();
+        let mut screen_buffer = Texture::new(ctx, GAME_SIZE)?;
+
+        loop {
+            #[cfg(feature = "profiler")]
+            profile_scope!("frame");
+            ctx.clear_color(&mut screen_buffer, (0.3, 0.3, 0.8, 1.0))?;
+            screen_buffer.clear_depth(ctx)?;
+
+            if f(ctx, &mut screen_buffer, &mut s, &mut r, &mut c)? {
+                break Ok(());
+            }
+
+            ctx.draw(
+                &mut surface,
+                &screen_buffer,
+                (0, 0),
+                &DrawConfig {
+                    scale: (WINDOW_SCALE, WINDOW_SCALE),
+                    ..Default::default()
+                },
+            )?;
+            ctx.finalize_frame()?;
+            r.time.frame();
+        }
+    }
+}
+
+pub fn load_window_icon<P: AsRef<Path>>(path: P) -> Result<Icon, image::ImageError> {
+    let icon = image::open(path)?.to_rgba();
+    let icon_dimensions = icon.dimensions();
+    Ok(Icon::from_rgba(icon.into_raw(), icon_dimensions.0, icon_dimensions.1).unwrap())
+}
 
 pub fn run() -> Result<(), crow::Error> {
-    let icon = image::open("textures/window_icon.png").unwrap().to_rgba();
-    let icon_dimensions = icon.dimensions();
-    let icon = Icon::from_rgba(icon.into_raw(), icon_dimensions.0, icon_dimensions.1).unwrap();
+    let icon = load_window_icon("textures/window_icon.png").unwrap();
 
     let mut ctx = Context::new(
         WindowBuilder::new()
@@ -53,29 +114,23 @@ pub fn run() -> Result<(), crow::Error> {
             .with_window_icon(Some(icon)),
         EventsLoop::new(),
     )?;
-    let mut surface = ctx.window_surface();
-    let mut screen_buffer = Texture::new(&mut ctx, GAME_SIZE)?;
 
-    let mut c = Components::new();
-    let mut r = Ressources::new(FPS);
-    let mut s = Systems::new();
+    let mut game = GlobalState::new(FPS);
 
     let config = config::EnvironmentConfig::load("ressources/environment.ron").unwrap();
 
-    init::player(&mut ctx, &mut c, &mut r)?;
+    init::player(&mut ctx, &mut game.c, &mut game.r)?;
 
-    let mut e = Some(environment::Environment::load(&mut ctx, &mut c, &config)?);
+    let mut e = Some(environment::Environment::load(
+        &mut ctx,
+        &mut game.c,
+        &config,
+    )?);
 
-    loop {
-        #[cfg(feature = "profiler")]
-        profile_scope!("frame");
-
+    game.run(&mut ctx, |ctx, screen_buffer, s, r, c| {
         if r.input_state.update(ctx.events_loop()) {
-            break;
+            return Ok(true);
         }
-
-        ctx.clear_color(&mut screen_buffer, (0.3, 0.3, 0.8, 1.0))?;
-        screen_buffer.clear_depth(&mut ctx)?;
 
         s.input_buffer.run(
             r.input_state.events(),
@@ -85,7 +140,7 @@ pub fn run() -> Result<(), crow::Error> {
 
         if r.input_state.down == input::ButtonState::Down {
             if let Some(e) = e.take() {
-                e.delete(&mut c);
+                e.delete(c);
             }
         }
 
@@ -119,7 +174,7 @@ pub fn run() -> Result<(), crow::Error> {
             &collisions,
         );
 
-        s.player.run(&mut c, &mut r, &collisions);
+        s.player.run(c, r, &collisions);
 
         // destruction timer
 
@@ -127,28 +182,16 @@ pub fn run() -> Result<(), crow::Error> {
             .run(&mut c.sprites, &mut c.animations, &mut r.animation_storage);
 
         draw::scene(
-            &mut ctx,
-            &mut screen_buffer,
+            ctx,
+            screen_buffer,
             &c.positions,
             &c.sprites,
             &c.depths,
             &c.mirrored,
             &c.colliders,
         )?;
-        draw::debug_colliders(&mut ctx, &mut screen_buffer, &c.positions, &c.colliders)?;
+        draw::debug_colliders(ctx, screen_buffer, &c.positions, &c.colliders)?;
 
-        ctx.draw(
-            &mut surface,
-            &screen_buffer,
-            (0, 0),
-            &DrawConfig {
-                scale: (WINDOW_SCALE, WINDOW_SCALE),
-                ..Default::default()
-            },
-        )?;
-        ctx.finalize_frame()?;
-        r.time.frame();
-    }
-
-    Ok(())
+        Ok(false)
+    })
 }
