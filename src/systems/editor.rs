0ddde2821eee32c1332ab2a876e5b7b2328f6c5a
Bastian Kauschke <bastian_kauschke@hotmail.de>
2020-02-22 13:55:36 +0100

    refactor editor

--- src/systems/editor.rs
+++ src/systems/editor.rs
@@ -1,3 +1,5 @@
+use std::convert::TryInto;
+
 use crow::Context;
 
 use crow_ecs::Joinable;
@@ -11,15 +13,36 @@ use crate::{
 
 #[derive(Debug, Default)]
 pub struct EditorSystem {
-    tile: Option<Tile>,
+    tile: Tile,
 }
 
 impl EditorSystem {
     pub fn new() -> Self {
-        EditorSystem {
-            tile: Some(Tile::Solid),
-        }
+        EditorSystem { tile: Tile::Solid }
     }
+}
+
+fn tile_on_click(c: &mut Components, r: &mut Ressources) -> ((i32, i32), (usize, usize)) {
+    let pos = r.input_state.cursor_position();
+
+    let (&c_pos, _) = (&c.positions, &c.cameras).join().unique().expect("Camera");
+    let c_pos: (i32, i32) = c_pos.into();
+
+    let ingame_pos = (pos.0 + c_pos.0, pos.1 + c_pos.1);
+
+    let chunk = (
+        ingame_pos.0 / CHUNK_WIDTH as i32,
+        ingame_pos.1 / CHUNK_HEIGHT as i32,
+    );
+    let tile = (
+        (ingame_pos.0 - chunk.0 * CHUNK_WIDTH as i32) / TILE_SIZE as i32,
+        (ingame_pos.1 - chunk.1 * CHUNK_HEIGHT as i32) / TILE_SIZE as i32,
+    );
+
+    (
+        chunk,
+        (tile.0.try_into().unwrap(), tile.1.try_into().unwrap()),
+    )
 }
 
 impl EditorSystem {
@@ -34,35 +57,20 @@ impl EditorSystem {
             .contains(&InputEvent::KeyDown(r.config.input.editor_tile))
         {
             self.tile = match self.tile {
-                None => Some(Tile::Solid),
-                Some(Tile::Solid) => Some(Tile::Spike),
-                Some(Tile::Spike) => Some(Tile::Bridge),
-                Some(Tile::Bridge) => Some(Tile::Grass),
-                Some(Tile::Grass) => None,
+                Tile::Solid => Tile::Spike,
+                Tile::Spike => Tile::Bridge,
+                Tile::Bridge => Tile::Grass,
+                Tile::Grass => Tile::Solid,
             };
             info!("Set editor tile to {:?}", self.tile);
         }
 
         if r.input_state.mouse(MouseButton::Left) == KeyState::Down {
-            let pos = r.input_state.cursor_position();
-
-            let (&c_pos, _) = (&c.positions, &c.cameras).join().unique().expect("Camera");
-            let c_pos: (i32, i32) = c_pos.into();
-
-            let ingame_pos = (pos.0 + c_pos.0, pos.1 + c_pos.1);
-
-            let chunk = (
-                ingame_pos.0 / CHUNK_WIDTH as i32,
-                ingame_pos.1 / CHUNK_HEIGHT as i32,
-            );
-            let tile = (
-                (ingame_pos.0 - chunk.0 * CHUNK_WIDTH as i32) / TILE_SIZE as i32,
-                (ingame_pos.1 - chunk.1 * CHUNK_HEIGHT as i32) / TILE_SIZE as i32,
-            );
+            let (chunk, tile) = tile_on_click(c, r);
 
             let chunk_pos = r.world.chunks.iter().position(|c| c.position == chunk);
             if let Some(chunk) = chunk_pos.map(|pos| &mut r.world.chunks[pos]) {
-                chunk.data.tiles[tile.1 as usize][tile.0 as usize] = self.tile;
+                chunk.data.tiles[tile.1 as usize][tile.0 as usize] = Some(self.tile);
                 chunk.rebuild(ctx, c)?;
             } else {
                 error!("Tried to edit an nonexisting chunk");
