12900229797a38babb834e91753f29fe40d38b77
Bastian Kauschke <bastian_kauschke@hotmail.de>
2020-02-24 23:24:54 +0100

    editor

--- src/systems/editor.rs
+++ src/systems/editor.rs
@@ -6,7 +6,7 @@ use crow_ecs::Joinable;
 
 use crate::{
     data::{Components, Position},
-    environment::{Tile, CHUNK_HEIGHT, CHUNK_WIDTH, TILE_SIZE},
+    environment::{chunk::Chunk, Tile, CHUNK_HEIGHT, CHUNK_TILES, CHUNK_WIDTH, TILE_SIZE},
     input::{InputEvent, Key, KeyState, MouseButton},
     ressources::Ressources,
 };
@@ -34,12 +34,18 @@ fn tile_on_click(camera: Position, r: &mut Ressources) -> ((i32, i32), (usize, u
         ingame_pos.1 / CHUNK_HEIGHT as i32 - (ingame_pos.1 < 0) as i32,
     );
 
-    let tile = (
-        (ingame_pos.0 % CHUNK_WIDTH as i32 + (ingame_pos.0 < 0) as i32 * CHUNK_WIDTH as i32)
-            / TILE_SIZE as i32,
-        (ingame_pos.1 % CHUNK_HEIGHT as i32 + (ingame_pos.1 < 0) as i32 * CHUNK_HEIGHT as i32)
-            / TILE_SIZE as i32,
+    let mut tile = (
+        (ingame_pos.0.abs() % CHUNK_WIDTH as i32) / TILE_SIZE as i32,
+        (ingame_pos.1.abs() % CHUNK_WIDTH as i32) / TILE_SIZE as i32,
     );
+
+    if ingame_pos.0 < 0 {
+        tile.0 = CHUNK_TILES as i32 - 1 - tile.0;
+    }
+
+    if ingame_pos.1 < 0 {
+        tile.1 = CHUNK_TILES as i32 - 1 - tile.1;
+    }
 
     (
         chunk,
@@ -89,12 +95,14 @@ impl EditorSystem {
             let (chunk, tile) = tile_on_click(camera, r);
 
             let chunk_pos = r.world.chunks.iter().position(|c| c.position == chunk);
-            if let Some(chunk) = chunk_pos.map(|pos| &mut r.world.chunks[pos]) {
-                chunk.data.tiles[tile.1 as usize][tile.0 as usize] = Some(self.tile);
-                chunk.rebuild(ctx, c)?;
+            let chunk = if let Some(chunk) = chunk_pos.map(|pos| &mut r.world.chunks[pos]) {
+                chunk
             } else {
-                error!("Tried to edit an nonexisting chunk");
-            }
+                r.world.chunks.push(Chunk::empty(chunk, c));
+                r.world.chunks.last_mut().unwrap()
+            };
+            chunk.data.tiles[tile.1 as usize][tile.0 as usize] = Some(self.tile);
+            chunk.rebuild(ctx, c)?;
         } else if r.input_state.mouse(MouseButton::Right) == KeyState::Down {
             let (chunk, tile) = tile_on_click(camera, r);
 
@@ -102,8 +110,6 @@ impl EditorSystem {
             if let Some(chunk) = chunk_pos.map(|pos| &mut r.world.chunks[pos]) {
                 chunk.data.tiles[tile.1 as usize][tile.0 as usize] = None;
                 chunk.rebuild(ctx, c)?;
-            } else {
-                error!("Tried to edit an nonexisting chunk");
             }
         }
 
